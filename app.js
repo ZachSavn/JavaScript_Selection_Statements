b257cdc375ede1a485692337f52df00cce165fae
zachary webb <zachsavn@gmail.com>
2022-06-21 10:46:34 -0400

    added comments

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let numAnswer = Math.floor(Math.random()*11);
 console.log(numAnswer);
 let guess = prompt("Care to play a guessing game? If so, please enter a number between 1-10.");
 
+/* This is an if/else statement. If the guess is less than the number answer, it will alert "Too low!"
+If the guess is greater than the number answer, it will alert "Too high!" If the guess is equal to
+the number answer, it will alert "Congratulations!!!" */
 if(guess < numAnswer) {
     alert("Too low!");
 } else if (guess > numAnswer) {
